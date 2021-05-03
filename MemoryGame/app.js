b84f3b96182e2ab8853469d7ae527c0cbe93f5fc
Madi <72963376+Madi-S@users.noreply.github.com>
2021-05-03 17:19:23 +0600

    images + partial code

---
+++ MemoryGame/app.js
@@ -0,0 +1,50 @@
+class MemoryGame {
+
+    srcs = [null, 'jenos.png', 'lian.png', 'dredge.png', 'maldamba']
+
+    constructor () {
+        this.container = document.querySelector('.container')
+    }
+    check() {
+
+    }
+    win() {
+
+    }
+    lose() {
+
+    }
+
+    makeCardClickable(card) {
+        card.addEventListener('click', (event) => {
+            card.src = 'images/' + this.srcs[card.id]
+        })
+    }
+
+    hideCardsIn(ms = 1000) {
+        setTimeout(() => {
+            const imgs = this.container.querySelectorAll('img')
+            for (let img of imgs) {
+                img.src = 'images/back.png'
+                this.makeCardClickable(img)
+            }
+        }, ms)
+    }
+
+    initCards() {
+      this.container.innerHTML = `
+      <img id="1" src="images/jenos.png" class="img-fluid" height="180" width="300">
+      <img id="2" src="images/lian.png" class="img-fluid" height="180" width="300">
+      <img id="3" src="images/dredge.png" class="img-fluid" height="180" width="300">
+      <img id="4" src="images/maldamba.png" class="img-fluid" height="180" width="300">
+      <img id="-1" src="images/jenos.png" class="img-fluid" height="180" width="300">
+      <img id="-2" src="images/lian.png" class="img-fluid" height="180" width="300">
+      <img id="-3" src="images/dredge.png" class="img-fluid" height="180" width="300">
+      <img id="-4" src="images/maldamba.png" class="img-fluid" height="180" width="300">
+      `
+    }
+}
+
+const g = new MemoryGame()
+g.initCards()
+g.hideCardsIn(5000)
